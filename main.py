1a76f65de8b8b4aaf2578b3ae4772cd973377f7b
GibNub <87067148+GibNub@users.noreply.github.com>
2022-09-05 18:35:02 +1200

    complete

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
+import threading
+
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-def main():
-    br = webdriver.Chrome(ChromeDriverManager().install())
-    br.get('https://yaksiege-1.johnnyl19432.repl.co/')
-    # Credentials
-    username = 'JohnnyLbozo'
-    password = 'AVeryLongPassword' # impliment random pass generator
-    kingdom = 'OhNoYouHaveVirus'
-    iteration = 220
+# Own modules
+import credentials
+
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    dr = webdriver.Chrome(options=options)
+    return dr
+
+
+def create_account(url, dr, username, kingdom):
+    dr.get(url)
     while True:
-        iteration += 1
+        # Credentials
+        password = credentials.password()
         # Goto signup
-        br.find_element(By.LINK_TEXT, 'Signup').click()
+        dr.find_element(By.ID, 'SignupButton').click()
         # Fill form
-        br.find_element(By.NAME, 'username').send_keys(f'{username}{str(iteration)}')
-        br.find_element(By.NAME, 'password').send_keys(password)
-        br.find_element(By.NAME, 'repeat-password').send_keys(password)
-        br.find_element(By.CLASS_NAME, 'submit-button').click()
-        # create kingdom
-        br.find_element(By.LINK_TEXT, 'Siege!').click()
-        br.find_element(By.NAME, 'kingdom_name').send_keys(f'{kingdom}{str(iteration)}')
-        br.find_element(By.CLASS_NAME, 'submit-button').click()
-        # logout
-        br.find_element(By.LINK_TEXT, 'Logout').click()
+        dr.find_element(By.NAME, 'username').send_keys(f'{username}{credentials.filler(len(username))}')
+        dr.find_element(By.NAME, 'password').send_keys(password)
+        dr.find_element(By.NAME, 'repeat-password').send_keys(password)
+        dr.find_element(By.CLASS_NAME, 'submit-button').click()
+        # Create kingdom
+        dr.find_element(By.LINK_TEXT, 'Siege!').click()
+        dr.find_element(By.NAME, 'kingdom_name').send_keys(f'{kingdom}{credentials.filler(len(kingdom))}')
+        dr.find_element(By.CLASS_NAME, 'submit-button').click()
+        # Logout
+        dr.find_element(By.LINK_TEXT, 'Logout').click()
+        print('Account Created')
+
+
+def check_driver(url, username, kingdom, dr=None):
+    if dr:
+        create_account(url, dr, username, kingdom)
+    else:
+        dr = get_driver()
+        create_account(url, dr, username, kingdom)
+        dr.quit()
+
+
+def main():
+    username = 'JohnnyLbozo'
+    kingdom = 'OkIPullUp'
+    url = 'https://yaksiege-1.johnnyl19432.repl.co/'
+    t = input("Threads? (leave blank for no) >>> ")
+    if t:
+        threads = []
+        count = int(input("How many? >>> "))
+        for i in range(20):
+            th = threading.Thread(target=check_driver, args=(url, username, kingdom))
+            th.daemon = True
+            threads.append(th)
+        for th in threads:
+            th.start()
+        for th in threads:
+            th.join()
+    else:
+        check_driver(url, username, kingdom)
+
 
 if __name__ == '__main__':
     main()
