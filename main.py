0126c565bcf023d23493dc9c768779d5148512ff
GibNub <87067148+GibNub@users.noreply.github.com>
2022-09-05 19:20:32 +1200

    whitespace

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def filler(length):
     x = ''.join(random.choices(string.ascii_letters + string.digits, k=(20-length)))
     return x
 
+
 def password():
     p = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(30))
     return p
